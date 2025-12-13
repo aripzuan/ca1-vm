e10486a5aef0c248a60665f93b7c16652de00c7a
aripzuan <arridz03@gmail.com>
2025-12-13 16:17:47 +0000

    trigger cloud build testing

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("CA1 VM Deployment is running successfully 🚀");
+  res.send("CA1 VM deployment via Cloud Build is LIVE 🚀");
 });
 
 app.get("/health", (req, res) => {
